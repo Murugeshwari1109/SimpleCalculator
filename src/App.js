ab69fa7fc4dd48a596556da552a13015b6ea2784
MURUGESHWARI200 <murugeshwarimurugan119@gmail.com>
2024-07-01 23:29:14 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ function App() {
     // style={{background:'linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12)'}}
     >
       <div className="text-3xl font-bold mb-4 text-gray-700">Simple Calculator</div>
-      <div className="w-80 h-[8vh]  p-3 text-3xl rounded-lg bg-gray-200 mb-4" >
+      <div className="w-80 h-[8vh]  p-3 text-3xl rounded-lg bg-gray-400 mb-4" >
         <span>{input}</span><span>{result}</span>
       </div>
-      <div className="bg-gray-700 p-6 rounded-lg shadow-lg w-80">
+      <div className="bg-gray-600 p-6 rounded-lg shadow-lg w-80">
         <div className="grid grid-cols-4 gap-2 mb-4">
           <button className="bg-gray-200 p-4 rounded-lg" onClick={handleClear}>Clear</button>
           <button className="bg-gray-200 p-4 rounded-lg" onClick={handleBackspace}>X</button>
@@ -48,7 +48,7 @@ function App() {
         </div>
         <div className="grid grid-cols-4 gap-2 mb-4">
           <button className="bg-gray-200 p-4 rounded-lg" onClick={handleClick('4')}>4</button>
-          <button className="bg-gray-200 p-4 rounded-lg" onClick={handleClick('3')}>5</button>
+          <button className="bg-gray-200 p-4 rounded-lg" onClick={handleClick('5')}>5</button>
           <button className="bg-gray-200 p-4 rounded-lg" onClick={handleClick('2')}>6</button>
           <button className="bg-yellow-500 text-white p-4 rounded-lg" onClick={handleClick('*')}>*</button>
         </div>
